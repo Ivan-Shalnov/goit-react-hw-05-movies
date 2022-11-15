cea21ddbda575ccb901dc61f82a85a4322ea52bf
Ivan-Shalnov <98489312+Ivan-Shalnov@users.noreply.github.com>
2022-11-15 01:16:07 +0100

    fix basename

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import './index.css';
-// basename = '/goit-react-hw-05-movies/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <SkeletonTheme baseColor="#202020" highlightColor="#444">
         <App />
       </SkeletonTheme>
